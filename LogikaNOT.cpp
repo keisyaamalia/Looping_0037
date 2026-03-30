02cf84373589b364011a8a740d40794f76391fe4
keisyaamalia <kkaysaamalia@gmail.com>
2026-03-30 17:08:50 +0700

    input nilai dan hitungan rata-rata

---
+++ LogikaNOT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB + nilM) / 2;
+
+    cout<<"Nilai rata-rata = "<<rerata<<endl;
+}
